370721bd4382d13a2fce4c606eeaa7109bfdc1ea
codedek <code_dek@hotmail.com>
2018-01-25 19:20:42 -0400

    Added the ability to search for the properties of a certain key by using a regular expression pattern

--- CodeDek.Ini.Tests/SectionTests.cs
+++ CodeDek.Ini.Tests/SectionTests.cs
@@ -4,237 +4,245 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CodeDek.Ini.Tests
 {
-    [TestClass]
-    public class SectionTests
+  [TestClass]
+  public class SectionTests
+  {
+    private Section _s;
+    string _ini;
+
+    public TestContext TestContext { get; set; }
+
+    [TestInitialize]
+    public void Setup()
     {
-        private Section _s;
-        string _ini;
-
-        public TestContext TestContext { get; set; }
-
-        [TestInitialize]
-        public void Setup()
-        {
-            _s = new Section("sec");
-            _s.Add(new Property("key", "val"));
-            _s.Add(new Property("key1", "val1"));
-            _s.Add(new Property("kEy2", "val2"));
-            _ini = _s.ToString();
-            //_s.Comment.Add("This is a section comment.");
-            //p.Comment.Add("This is a property comment.");
-        }
-
-        [TestMethod]
-        public void Section_PropertiesIgnoreCaseIsFilter_ReturnsThree()
-        {
-            _s.Add(new Property("key", "val1"));
-            _s.Add(new Property("KEY", "val3"));
-            Assert.AreEqual(3, _s.Properties(Filter.IgnoreCaseIs, "key").Count());
-        }
-
-        [TestMethod]
-        public void Section_PropertiesIsFilter_ReturnsTwo()
-        {
-            _s.Add(new Property("key", "val1"));
-            _s.Add(new Property("KEY", "val3"));
-            Assert.AreEqual(2, _s.Properties(Filter.Is, "key").Count());
-        }
-
-        [TestMethod]
-        public void Section_PropertiesIgnoreCaseContainsFilter_ReturnsThree()
-        {
-            Assert.AreEqual(3, _s.Properties(Filter.IgnoreCaseContains, "Y").Count());
-        }
-
-        [TestMethod]
-        public void Section_PropertiesContainsFilter_ReturnsOne()
-        {
-            Assert.AreEqual(1, _s.Properties(Filter.Contains, "E").Count());
-        }
-
-        [TestMethod]
-        public void Section_PropertiesIgnoreCaseEndsWithFilter_ReturnsOne()
-        {
-            Assert.AreEqual(1, _s.Properties(Filter.IgnoreCaseEndsWith, "Y").Count());
-        }
-
-        [TestMethod]
-        public void Section_PropertiesEndsWithFilter_ReturnsOne()
-        {
-            Assert.AreEqual(1, _s.Properties(Filter.EndsWith, "2").Count());
-        }
-
-        [TestMethod]
-        public void Section_PropertiesIgnoreCaseStartsWithFilter_ReturnsThree()
-        {
-            Assert.AreEqual(3, _s.Properties(Filter.IgnoreCaseStartsWith, "kEy").Count());
-        }
-
-        [TestMethod]
-        public void Section_PropertiesStartsWithFilter_ReturnsOne()
-        {
-            Assert.AreEqual(1, _s.Properties(Filter.StartsWith, "kEy").Count());
-        }
-
-        [TestMethod]
-        public void
-            Section_WhenParseStringWithOneSectionAndTwoProperties_ReturnsPropertyCountOfTwoAndToStringOfSecKeyEqualsValAndKeyOneEqualsValOne()
-        {
-            var actual = Section.Parse(_s.ToString());
-            Assert.AreEqual(3, actual.Properties().Count());
-            Assert.AreEqual(_ini, actual.ToString());
-        }
-
-        [TestMethod]
-        public void Section_WhenInstintiatedWithAnotherSection_CreatesAProperCopy()
-        {
-            var s = new Section(_s);
-            Assert.AreNotEqual(s, _s);
-        }
-
-        [TestMethod]
-        public void Section_WhenInstantiatedWithInvalidName_ThrowsArgumentException()
-        {
-            Section sec = null;
-            Assert.ThrowsException<ArgumentException>(() => new Section(sec));
-            Assert.ThrowsException<ArgumentException>(() => new Section(""));
-        }
-
-        [TestMethod]
-        public void Section_PropertyWithArgsKeyAndIgnoreCaseTrue_ReturnsVal()
-        {
-            Assert.AreEqual("val", _s.Property("Key", true).Value);
-        }
-
-        [TestMethod]
-        public void Section_PropertyWithArgsKeyAndIgnoreCaseFalse_ReturnsVal1()
-        {
-            _s.Add(new Property("Key", "val1"));
-            Assert.AreEqual("val1", _s.Property("Key", false).Value);
-        }
-
-        [TestMethod]
-        public void Section_FindPropertyByKey_ReturnsPropertyIfExists()
-        {
-            var expected = "kEy2=val2";
-            Assert.AreEqual(expected, _s.Property("key2").ToString());
-        }
-
-        [TestMethod]
-        public void Section_FindPropertyByKeyAndValue_ReturnsPropertyIfExists()
-        {
-            var expected = "kEy2=val2";
-            Assert.AreEqual(expected, _s.Property("key2", "val2").ToString());
-        }
-
-        [TestMethod]
-        public void Section_WhenPropertyIsPassedInRemove_PropertyIsRemovedIfFound()
-        {
-            var p = _s.Property("key2", "val2");
-            _s.Remove(p);
-            Assert.AreEqual(2, _s.Properties().Count());
-        }
-
-        [TestMethod]
-        public void Section_WhenKeyAndValueIsPassedInRemove_PropertyIsRemovedIfFound()
-        {
-            // todo: make value optional for remove
-            _s.Remove("key2", "val2");
-            Assert.AreEqual(2, _s.Properties().Count());
-        }
-
-        [TestMethod]
-        public void Section_AddPropertyOptionIfKeyAndValueIsUniqueOnAUniqueProperty_PropertiesCountIsFour()
-        {
-            _s.Add(new Property("key3", "val3"));
-            Assert.AreEqual(4, _s.Properties().Count());
-        }
-
-        [TestMethod]
-        public void Section_AddPropertyOptionIfKeyAndValueIsUniqueOnAUniqueValueButNotKey_PropertiesCountIsThree()
-        {
-            _s.Add(new Property("key2", "val3"));
-            TestContext.WriteLine(_s.ToString());
-            Assert.AreEqual(4, _s.Properties().Count());
-        }
-
-        [TestMethod]
-        public void Section_AddPropertyOptionIfKeyIsUniqueOnAUniqueKeyProperty_PropertiesCountIsFour()
-        {
-            _s.Add(new Property("key3", "val3"), PropertyAddOption.KeyIsUnique);
-            TestContext.WriteLine(_s.ToString());
-            Assert.AreEqual(4, _s.Properties().Count());
-        }
-
-        [TestMethod]
-        public void Section_AddPropertyOptionIfKeyIsUniqueOnANonUniqueKeyProperty_PropertiesCountIsThree()
-        {
-            _s.Add(new Property("key2", "val3"), PropertyAddOption.KeyIsUnique);
-            TestContext.WriteLine(_s.ToString());
-            Assert.AreEqual(3, _s.Properties().Count());
-        }
-
-        [TestMethod]
-        public void Section_AddPropertyOptionUpdateValueIfPropertyExistOnUniqueProperty_PropertiesCountIsFour()
-        {
-            _s.Add(new Property("key4", "val4"), PropertyAddOption.Overwrite);
-            TestContext.WriteLine(_s.ToString());
-            Assert.AreEqual(4, _s.Properties().Count());
-        }
-
-        [TestMethod]
-        public void Section_AddPropertyOptionUpdateValueIfPropertyExistOnExistingProperty_PropertiesCountIsThree()
-        {
-            _s.Add(new Property("key", "val++"), PropertyAddOption.Overwrite);
-            TestContext.WriteLine(_s.ToString());
-            Assert.AreEqual(3, _s.Properties().Count());
-        }
-
-        [TestMethod]
-        public void Section_ParseEmptyString_ReturnsNullSection()
-        {
-            Assert.IsNull(Section.Parse(""));
-        }
-
-        [TestMethod]
-        public void Section_ParseNullString_ReturnsNullSection()
-        {
-            Assert.IsNull(Section.Parse(null));
-        }
-
-        //[TestMethod]
-        //public void Section_WhenCreate_StringWithCommentsRemoved()
-        //{
-        //    var expected = $"[NewSection]{Environment.NewLine}"
-        //                 + "Jon=Doe";
-        //    _s.Comment.Clear();
-        //    _s.Properties.ToList().ForEach(p => p.Comment.Clear());
-        //    Assert.AreEqual(expected, _s.String);
-        //}
-
-        //[TestMethod]
-        //public void Section_WhenAddDisabledPropertyThenDisableAndEnableSection_PropertyIsStillDisabled()
-        //{
-        //    // prerequisites
-        //    _s.Comment.Clear();
-        //    _s.Properties.ToList().ForEach(p => p.Comment.Clear());
-        //    _s.Properties.Remove(_s.Property("Jon"));
-
-        //    // create disabled property
-        //    var disabledProperty = new Property("enabled", "false");
-        //    disabledProperty.IsEnable = false;
-        //    var expected = "#disabledProperty: enabled=false";
-
-        //    // add it to enabled section then disable section
-        //    _s.Properties.Add(disabledProperty);
-        //    _s.IsEnabled = false;
-        //    var expected2 = $"#disabledSection: [NewSection]{Environment.NewLine}#disabledSection: {expected}";
-        //    Assert.AreEqual(expected2, _s.String);
-
-        //    // re - enable the section
-        //    _s.IsEnabled = true;
-        //    var expected3 = $"[NewSection]{Environment.NewLine}{expected}";
-        //    Assert.AreEqual(expected3, _s.String);
-        //}
+      _s = new Section("sec");
+      _s.Add(new Property("key", "val"));
+      _s.Add(new Property("key1", "val1"));
+      _s.Add(new Property("kEy2", "val2"));
+      _ini = _s.ToString();
+      //_s.Comment.Add("This is a section comment.");
+      //p.Comment.Add("This is a property comment.");
     }
+
+    [TestMethod]
+    public void Section_PropertiesRegxSearchForKeyWithNumber_ReturnsTwo()
+    {
+      var actual = _s.Properties(@"\d+$").ToList();
+      TestContext.WriteLine($"1: {actual[0].Key} | 2: {actual[1].Key}");
+      Assert.AreEqual(2, actual.Count);
+    }
+
+    [TestMethod]
+    public void Section_PropertiesIgnoreCaseIsFilter_ReturnsThree()
+    {
+      _s.Add(new Property("key", "val1"));
+      _s.Add(new Property("KEY", "val3"));
+      Assert.AreEqual(3, _s.Properties(Filter.IgnoreCaseIs, "key").Count());
+    }
+
+    [TestMethod]
+    public void Section_PropertiesIsFilter_ReturnsTwo()
+    {
+      _s.Add(new Property("key", "val1"));
+      _s.Add(new Property("KEY", "val3"));
+      Assert.AreEqual(2, _s.Properties(Filter.Is, "key").Count());
+    }
+
+    [TestMethod]
+    public void Section_PropertiesIgnoreCaseContainsFilter_ReturnsThree()
+    {
+      Assert.AreEqual(3, _s.Properties(Filter.IgnoreCaseContains, "Y").Count());
+    }
+
+    [TestMethod]
+    public void Section_PropertiesContainsFilter_ReturnsOne()
+    {
+      Assert.AreEqual(1, _s.Properties(Filter.Contains, "E").Count());
+    }
+
+    [TestMethod]
+    public void Section_PropertiesIgnoreCaseEndsWithFilter_ReturnsOne()
+    {
+      Assert.AreEqual(1, _s.Properties(Filter.IgnoreCaseEndsWith, "Y").Count());
+    }
+
+    [TestMethod]
+    public void Section_PropertiesEndsWithFilter_ReturnsOne()
+    {
+      Assert.AreEqual(1, _s.Properties(Filter.EndsWith, "2").Count());
+    }
+
+    [TestMethod]
+    public void Section_PropertiesIgnoreCaseStartsWithFilter_ReturnsThree()
+    {
+      Assert.AreEqual(3, _s.Properties(Filter.IgnoreCaseStartsWith, "kEy").Count());
+    }
+
+    [TestMethod]
+    public void Section_PropertiesStartsWithFilter_ReturnsOne()
+    {
+      Assert.AreEqual(1, _s.Properties(Filter.StartsWith, "kEy").Count());
+    }
+
+    [TestMethod]
+    public void
+      Section_WhenParseStringWithOneSectionAndTwoProperties_ReturnsPropertyCountOfTwoAndToStringOfSecKeyEqualsValAndKeyOneEqualsValOne()
+    {
+      var actual = Section.Parse(_s.ToString());
+      Assert.AreEqual(3, actual.Properties().Count());
+      Assert.AreEqual(_ini, actual.ToString());
+    }
+
+    [TestMethod]
+    public void Section_WhenInstintiatedWithAnotherSection_CreatesAProperCopy()
+    {
+      var s = new Section(_s);
+      Assert.AreNotEqual(s, _s);
+    }
+
+    [TestMethod]
+    public void Section_WhenInstantiatedWithInvalidName_ThrowsArgumentException()
+    {
+      Section sec = null;
+      Assert.ThrowsException<ArgumentException>(() => new Section(sec));
+      Assert.ThrowsException<ArgumentException>(() => new Section(""));
+    }
+
+    [TestMethod]
+    public void Section_PropertyWithArgsKeyAndIgnoreCaseTrue_ReturnsVal()
+    {
+      Assert.AreEqual("val", _s.Property("Key", true).Value);
+    }
+
+    [TestMethod]
+    public void Section_PropertyWithArgsKeyAndIgnoreCaseFalse_ReturnsVal1()
+    {
+      _s.Add(new Property("Key", "val1"));
+      Assert.AreEqual("val1", _s.Property("Key", false).Value);
+    }
+
+    [TestMethod]
+    public void Section_FindPropertyByKey_ReturnsPropertyIfExists()
+    {
+      var expected = "kEy2=val2";
+      Assert.AreEqual(expected, _s.Property("key2").ToString());
+    }
+
+    [TestMethod]
+    public void Section_FindPropertyByKeyAndValue_ReturnsPropertyIfExists()
+    {
+      var expected = "kEy2=val2";
+      Assert.AreEqual(expected, _s.Property("key2", "val2").ToString());
+    }
+
+    [TestMethod]
+    public void Section_WhenPropertyIsPassedInRemove_PropertyIsRemovedIfFound()
+    {
+      var p = _s.Property("key2", "val2");
+      _s.Remove(p);
+      Assert.AreEqual(2, _s.Properties().Count());
+    }
+
+    [TestMethod]
+    public void Section_WhenKeyAndValueIsPassedInRemove_PropertyIsRemovedIfFound()
+    {
+      // todo: make value optional for remove
+      _s.Remove("key2", "val2");
+      Assert.AreEqual(2, _s.Properties().Count());
+    }
+
+    [TestMethod]
+    public void Section_AddPropertyOptionIfKeyAndValueIsUniqueOnAUniqueProperty_PropertiesCountIsFour()
+    {
+      _s.Add(new Property("key3", "val3"));
+      Assert.AreEqual(4, _s.Properties().Count());
+    }
+
+    [TestMethod]
+    public void Section_AddPropertyOptionIfKeyAndValueIsUniqueOnAUniqueValueButNotKey_PropertiesCountIsThree()
+    {
+      _s.Add(new Property("key2", "val3"));
+      TestContext.WriteLine(_s.ToString());
+      Assert.AreEqual(4, _s.Properties().Count());
+    }
+
+    [TestMethod]
+    public void Section_AddPropertyOptionIfKeyIsUniqueOnAUniqueKeyProperty_PropertiesCountIsFour()
+    {
+      _s.Add(new Property("key3", "val3"), PropertyAddOption.KeyIsUnique);
+      TestContext.WriteLine(_s.ToString());
+      Assert.AreEqual(4, _s.Properties().Count());
+    }
+
+    [TestMethod]
+    public void Section_AddPropertyOptionIfKeyIsUniqueOnANonUniqueKeyProperty_PropertiesCountIsThree()
+    {
+      _s.Add(new Property("key2", "val3"), PropertyAddOption.KeyIsUnique);
+      TestContext.WriteLine(_s.ToString());
+      Assert.AreEqual(3, _s.Properties().Count());
+    }
+
+    [TestMethod]
+    public void Section_AddPropertyOptionUpdateValueIfPropertyExistOnUniqueProperty_PropertiesCountIsFour()
+    {
+      _s.Add(new Property("key4", "val4"), PropertyAddOption.Overwrite);
+      TestContext.WriteLine(_s.ToString());
+      Assert.AreEqual(4, _s.Properties().Count());
+    }
+
+    [TestMethod]
+    public void Section_AddPropertyOptionUpdateValueIfPropertyExistOnExistingProperty_PropertiesCountIsThree()
+    {
+      _s.Add(new Property("key", "val++"), PropertyAddOption.Overwrite);
+      TestContext.WriteLine(_s.ToString());
+      Assert.AreEqual(3, _s.Properties().Count());
+    }
+
+    [TestMethod]
+    public void Section_ParseEmptyString_ReturnsNullSection()
+    {
+      Assert.IsNull(Section.Parse(""));
+    }
+
+    [TestMethod]
+    public void Section_ParseNullString_ReturnsNullSection()
+    {
+      Assert.IsNull(Section.Parse(null));
+    }
+
+    //[TestMethod]
+    //public void Section_WhenCreate_StringWithCommentsRemoved()
+    //{
+    //    var expected = $"[NewSection]{Environment.NewLine}"
+    //                 + "Jon=Doe";
+    //    _s.Comment.Clear();
+    //    _s.Properties.ToList().ForEach(p => p.Comment.Clear());
+    //    Assert.AreEqual(expected, _s.String);
+    //}
+
+    //[TestMethod]
+    //public void Section_WhenAddDisabledPropertyThenDisableAndEnableSection_PropertyIsStillDisabled()
+    //{
+    //    // prerequisites
+    //    _s.Comment.Clear();
+    //    _s.Properties.ToList().ForEach(p => p.Comment.Clear());
+    //    _s.Properties.Remove(_s.Property("Jon"));
+
+    //    // create disabled property
+    //    var disabledProperty = new Property("enabled", "false");
+    //    disabledProperty.IsEnable = false;
+    //    var expected = "#disabledProperty: enabled=false";
+
+    //    // add it to enabled section then disable section
+    //    _s.Properties.Add(disabledProperty);
+    //    _s.IsEnabled = false;
+    //    var expected2 = $"#disabledSection: [NewSection]{Environment.NewLine}#disabledSection: {expected}";
+    //    Assert.AreEqual(expected2, _s.String);
+
+    //    // re - enable the section
+    //    _s.IsEnabled = true;
+    //    var expected3 = $"[NewSection]{Environment.NewLine}{expected}";
+    //    Assert.AreEqual(expected3, _s.String);
+    //}
+  }
 }
--- CodeDek.Ini/Section.cs
+++ CodeDek.Ini/Section.cs
@@ -5,119 +5,131 @@ using System.Text.RegularExpressions;
 
 namespace CodeDek.Ini
 {
-    public class Section
+  public class Section
+  {
+    const string _sectionPattern = @"^(?:\[(?<sectionName>.+)?\])(?:[\w\W](?!^\[.+\]))+$";
+    readonly List<Property> _properties = new List<Property>();
+
+    public string Name { get; }
+
+    public Section(string name)
     {
-        const string _sectionPattern = @"^(?:\[(?<sectionName>.+)?\])(?:[\w\W](?!^\[.+\]))+$";
-        readonly List<Property> _properties = new List<Property>();
+      if (string.IsNullOrWhiteSpace(name))
+        throw new System.ArgumentException("Please enter a valid name", nameof(name));
 
-        public string Name { get; }
-
-        public Section(string name)
-        {
-            if (string.IsNullOrWhiteSpace(name))
-                throw new System.ArgumentException("Please enter a valid name", nameof(name));
-
-            Name = name.Trim().TrimStart('[').TrimEnd(']').Trim();
-        }
-
-        public Section(Section other)
-        {
-            if (other == null)
-                throw new ArgumentException("Cannot pass a null section", nameof(other));
-
-            Name = other?.Name;
-            _properties = other?.Properties().ToList();
-        }
-
-        public bool Add(Property property, PropertyAddOption addOption = PropertyAddOption.PropertyIsUnique)
-        {
-            if (property == null) return false;
-
-            switch (addOption)
-            {
-                case PropertyAddOption.KeyIsUnique:
-                    if (Property(property.Key) == null) _properties.Add(property);
-
-                    return true;
-                case PropertyAddOption.PropertyIsUnique:
-                    if (Property(property.Key, property.Value) == null) _properties.Add(property);
-
-                    return true;
-                case PropertyAddOption.Overwrite:
-                    var index = _properties
-                        .FindIndex(p => p.Key.Equals(property.Key, StringComparison.OrdinalIgnoreCase));
-
-                    if (index <= -1)
-                    {
-                        _properties.Add(property);
-                        return true;
-                    } else
-                    {
-                        _properties.RemoveAt(index);
-                        _properties.Insert(index, property);
-                        return true;
-                    }
-            }
-
-            return false;
-        }
-
-        public static Section Parse(string text)
-        {
-            if (string.IsNullOrEmpty(text)) return default;
-
-            var s = Regex.Match(text.Trim(), _sectionPattern);
-            if (!s.Success) return default;
-
-            var sec = new Section(s.Groups["sectionName"].Value);
-            foreach (var l in s.Value.SplitToLines())
-                sec.Add(CodeDek.Ini.Property.Parse(l));
-            return sec;
-        }
-
-        public void Clear() => _properties.Clear();
-
-        public bool Remove(Property property) => _properties.Remove(property);
-
-        public void Remove(string key, string value) => _properties.Remove(Property(key, value));
-
-        public Property Property(string key, bool ignoreCase = true) =>
-            _properties.Find(p => ignoreCase
-                                 ? p.Key.Equals(key, StringComparison.OrdinalIgnoreCase)
-                                 : p.Key.Equals(key));
-
-        public Property Property(string key, string value) =>
-            _properties.Find(p => p.Key.IgnoreCaseEquals(key) && p.Value.IgnoreCaseEquals(value));
-
-        public IEnumerable<Property> Properties() => _properties;
-
-        public IEnumerable<Property> Properties(Filter filterKey, string search)
-        {
-            switch (filterKey)
-            {
-                case Filter.Is:
-                    return _properties.Where(p => p.Key.IgnoreCaseEquals(search, false));
-                case Filter.StartsWith:
-                    return _properties.Where(p => p.Key.IgnoreCaseStartsWith(search, false));
-                case Filter.EndsWith:
-                    return _properties.Where(p => p.Key.IgnoreCaseEndsWith(search, false));
-                case Filter.Contains:
-                    return _properties.Where(p => p.Key.IgnoreCaseContains(search, false));
-                case Filter.IgnoreCaseIs:
-                    return _properties.Where(p => p.Key.IgnoreCaseEquals(search));
-                case Filter.IgnoreCaseStartsWith:
-                    return _properties.Where(p => p.Key.IgnoreCaseStartsWith(search));
-                case Filter.IgnoreCaseEndsWith:
-                    return _properties.Where(p => p.Key.IgnoreCaseEndsWith(search));
-                case Filter.IgnoreCaseContains:
-                    return _properties.Where(p => p.Key.IgnoreCaseContains(search));
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(filterKey), filterKey, null);
-            }
-        }
-
-        public override string ToString() =>
-            $"[{Name}]{Environment.NewLine}{string.Join(Environment.NewLine, _properties.FindAll(p => p.ToString() != ""))}"
-                .Trim();
+      Name = name.Trim().TrimStart('[').TrimEnd(']').Trim();
     }
+
+    public Section(Section other)
+    {
+      if (other == null)
+        throw new ArgumentException("Cannot pass a null section", nameof(other));
+
+      Name = other?.Name;
+      _properties = other?.Properties().ToList();
+    }
+
+    public bool Add(Property property, PropertyAddOption addOption = PropertyAddOption.PropertyIsUnique)
+    {
+      if (property == null) return false;
+
+      switch (addOption)
+      {
+        case PropertyAddOption.KeyIsUnique:
+          if (Property(property.Key) == null) _properties.Add(property);
+
+          return true;
+        case PropertyAddOption.PropertyIsUnique:
+          if (Property(property.Key, property.Value) == null) _properties.Add(property);
+
+          return true;
+        case PropertyAddOption.Overwrite:
+          var index = _properties
+            .FindIndex(p => p.Key.Equals(property.Key, StringComparison.OrdinalIgnoreCase));
+
+          if (index <= -1)
+          {
+            _properties.Add(property);
+            return true;
+          } else
+          {
+            _properties.RemoveAt(index);
+            _properties.Insert(index, property);
+            return true;
+          }
+      }
+
+      return false;
+    }
+
+    public static Section Parse(string text)
+    {
+      if (string.IsNullOrEmpty(text)) return default;
+
+      var s = Regex.Match(text.Trim(), _sectionPattern);
+      if (!s.Success) return default;
+
+      var sec = new Section(s.Groups["sectionName"].Value);
+      foreach (var l in s.Value.SplitToLines())
+        sec.Add(CodeDek.Ini.Property.Parse(l));
+      return sec;
+    }
+
+    public void Clear() => _properties.Clear();
+
+    public bool Remove(Property property) => _properties.Remove(property);
+
+    public void Remove(string key, string value) => _properties.Remove(Property(key, value));
+
+    public Property Property(string key, bool ignoreCase = true) =>
+      _properties.Find(p => ignoreCase
+                         ? p.Key.Equals(key, StringComparison.OrdinalIgnoreCase)
+                         : p.Key.Equals(key));
+
+    public Property Property(string key, string value) =>
+      _properties.Find(p => p.Key.IgnoreCaseEquals(key) && p.Value.IgnoreCaseEquals(value));
+
+    public IEnumerable<Property> Properties() => _properties;
+
+    public IEnumerable<Property> Properties(Filter filterKey, string search)
+    {
+      switch (filterKey)
+      {
+        case Filter.Is:
+          return _properties.Where(p => p.Key.IgnoreCaseEquals(search, false));
+        case Filter.StartsWith:
+          return _properties.Where(p => p.Key.IgnoreCaseStartsWith(search, false));
+        case Filter.EndsWith:
+          return _properties.Where(p => p.Key.IgnoreCaseEndsWith(search, false));
+        case Filter.Contains:
+          return _properties.Where(p => p.Key.IgnoreCaseContains(search, false));
+        case Filter.IgnoreCaseIs:
+          return _properties.Where(p => p.Key.IgnoreCaseEquals(search));
+        case Filter.IgnoreCaseStartsWith:
+          return _properties.Where(p => p.Key.IgnoreCaseStartsWith(search));
+        case Filter.IgnoreCaseEndsWith:
+          return _properties.Where(p => p.Key.IgnoreCaseEndsWith(search));
+        case Filter.IgnoreCaseContains:
+          return _properties.Where(p => p.Key.IgnoreCaseContains(search));
+        default:
+          throw new ArgumentOutOfRangeException(nameof(filterKey), filterKey, null);
+      }
+    }
+
+    public IEnumerable<Property> Properties(string regxPattern)
+    {
+      if (string.IsNullOrWhiteSpace(regxPattern)) yield return default;
+
+      foreach (var property in _properties)
+      {
+        var m = Regex.Match(property.Key, regxPattern);
+        if (m.Success)
+          yield return property;
+      }
+    }
+
+    public override string ToString() =>
+      $"[{Name}]{Environment.NewLine}{string.Join(Environment.NewLine, _properties.FindAll(p => p.ToString() != ""))}"
+        .Trim();
+  }
 }
